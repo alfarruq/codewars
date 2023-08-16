c1da4b5e474b96b603c07067799b9fb575f8158d
alfarruq <mengboyevfarrux01@gmail.com>
2023-08-16 13:35:37 +0500

    8kyu Even or Odd

---
+++ codewarse/exercise9.js
@@ -0,0 +1,11 @@
+// exercise => 8kyu
+
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+//<------solution-------->
+
+const evenOrOdd = (num)=>  num % 2 == 0 ? "Even": "Odd" 
+
+
+console.log(evenOrOdd(2));
