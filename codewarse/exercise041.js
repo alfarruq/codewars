c6da28733311dedeed6e78411e25bfcfbd5526bf
Farrux Mengboyev <135288285+alfarruq@users.noreply.github.com>
2023-09-03 19:12:05 +0300

    8kyu Convert boolean values to strings 'Yes' or 'No'

---
+++ codewarse/exercise041.js
@@ -0,0 +1,16 @@
+
+// <---------  exercise   --------> => 8kyu
+
+// Complete the method that takes a boolean value and return 
+// a "Yes" string for true, or a "No" string for false
+
+// <--------- Exsample   -------->
+// NO
+
+// <---------  solution  -------->
+
+function boolToWord( bool ){
+  return bool ? 'Yes': 'No';
+}
+console.log(boolToWord(true));
+
