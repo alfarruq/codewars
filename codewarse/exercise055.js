6748db18909561c6d32572b3c94c37976329eee4
alfarruq <mengboyevfarrux01@gmail.com>
2023-09-12 10:23:14 +0500

    5kyu Moving Zeros To The End

--- codewarse/exercise055.js
+++ codewarse/exercise055.js
@@ -1,27 +1,22 @@
 
+// <---------  exercise   --------> => 5kyu
 
-// <---------  exercise   --------> => 6kyu
-
-// You will be given an array of numbers. You have to sort the odd numbers
-// in ascending order while leaving the even numbers at their original positions.
+// Write an algorithm that takes an array and moves all of the zeros to the end, 
+// preserving the order of the other elements.
 
 
 // <--------- Exsample   -------->
-// [7, 1]  =>  [1, 7]
-// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
-// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) => returns[false,1,1,2,1,3,"a",0,0]
 
 // <---------  solution  -------->
 
-function sortArray(arr) {
-  let sortArr = arr.filter((v) => v % 2 == 1 || v % 2 == -1 ).sort((a, b) => a - b);
-  let count = -1;
-  let result = arr.map((v) => {
-    if (v % 2 == 1 || v % 2 == -1) {
-      count++;
-      return sortArr[count];
-    } else return v;
-  });
-  return result;
+function moveZeros(arr) {
+  let count = 0;
+  arr.forEach((v) => v === 0 && count++);;
+  let array = arr.filter((v) => v !== 0)
+  for (let i = 0; i < count; i++) array.push(0);
+
+  return array;
+  
 }
 
