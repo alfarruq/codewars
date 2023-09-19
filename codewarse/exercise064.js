d77f8ec1838a962bf0c33ee8b1a4c089cec4eedd
alfarruq <mengboyevfarrux01@gmail.com>
2023-09-19 11:12:05 +0500

    5kyu Scramblies

--- codewarse/exercise064.js
+++ codewarse/exercise064.js
@@ -1,11 +1,25 @@
 // <---------  exercise   --------> => 5kyu
 
-
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 
+// characters can be rearranged to match str2, otherwise returns false.
 
 // <---------  Exsample   -------->
 
-
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
 
 
 // <---------  solution  -------->
 
+function scramble(str1, str2) {
+    let newStr1 = str1;
+      
+      
+      for (let i = 0; i < str2.length; i++) {
+        if(!newStr1.includes(str2[i])) return false;
+        newStr1 = newStr1.replace(str2[i],'');
+      }
+    
+    return true;
+  }
