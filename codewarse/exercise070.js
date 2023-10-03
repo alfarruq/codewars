40196c2f8435fa276551f8e816f2946103916bff
alfarruq <mengboyevfarrux01@gmail.com>
2023-10-03 21:46:26 +0500

    6kyu Find the missing letter

--- codewarse/exercise070.js
+++ codewarse/exercise070.js
@@ -1,8 +1,20 @@
-// <---------  exercise   --------> => 5kyu
+// <---------  exercise   --------> => 6kyu
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
 
 // <---------  Exsample   -------->
 
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+
 // <---------  solution  -------->
 
-
-
+function findMissingLetter(array) {
+  return String.fromCharCode(
+    array
+      .find((c, i) => array[i + 1].charCodeAt() - c.charCodeAt() !== 1)
+      .charCodeAt() + 1
+  );
+}
