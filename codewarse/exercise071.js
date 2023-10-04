f8ee6c8efd63e95d236e2d335d2b83ca75c40ddf
alfarruq <mengboyevfarrux01@gmail.com>
2023-10-04 17:16:55 +0500

    6kyu Build Tower

--- codewarse/exercise071.js
+++ codewarse/exercise071.js
@@ -1,8 +1,17 @@
 // <---------  exercise   --------> => 5kyu
 
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+// For example, a tower with 3 floors looks like this:
+
 // <---------  Exsample   -------->
+
+// ["  *  ", " *** ", "*****"];
 
 // <---------  solution  -------->
 
-
-
+function towerBuilder(n) {
+  return [...Array(n)].map(
+    (_, i) =>
+      " ".repeat(n - 1 - i) + "*".repeat(i * 2 + 1) + " ".repeat(n - 1 - i)
+  );
+}
