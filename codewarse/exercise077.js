a0a4f3182a0966d4629ad439357d168339110fc6
‘alfarruq’ <mengboyevfarrux@gmail.com>
2024-02-15 17:51:57 +0500

    8kyu Reversed Strings

--- codewarse/exercise077.js
+++ codewarse/exercise077.js
@@ -1,9 +1,13 @@
 // <---------  exercise   --------> => 6kyu
 
+// Complete the solution so that it reverses the string passed into it.
 
 // <---------  Exsample   -------->
-
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
 
 // <---------  solution  -------->
 
-
+function solution(str) {
+  return str.split("").reverse().join("");
+}
