61be446c2e174fa8e5e60817886a9b211b42d6c2
‘alfarruq’ <mengboyevfarrux01@gmail.com>
2024-02-16 10:35:08 +0500

    8kyu Opposite number

---
+++ codewarse/exercise079.js
@@ -0,0 +1,18 @@
+// <---------  exercise   --------> => 
+
+// Very simple, given a number (integer / decimal / both depending on the language),
+// find its opposite (additive inverse).
+
+
+// <---------  exsample   -------->
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// <---------  solution  -------->
+
+const opposite=(num)=> -1 * num
+
+// console.log(opposite);
+
