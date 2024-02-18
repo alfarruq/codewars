16c586dc1dc0b55d003a40ef0585c789cb4e3bd5
‘alfarruq’ <mengboyevfarrux01@gmail.com>
2024-02-18 12:37:03 +0500

    7kyu Find the next perfect square!

---
+++ codewarse/exercise083.js
@@ -0,0 +1,18 @@
+// <---------  exercise   --------> =>
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+//  Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// <---------  exsample   -------->
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+// <---------  solution  -------->
+
+function findNextSquare(sq) {
+  let numSq = Math.sqrt(sq);
+  return numSq == Math.trunc(numSq) ? Math.pow(numSq + 1, 2) : -1;
+}
