84ca8da80e12538c8a3c9371bb4235f89cb23483
‘alfarruq’ <mengboyevfarrux01@gmail.com>
2024-02-18 12:39:13 +0500

    7kyu Find the next perfect square!

--- codewarse/exercise083.js
+++ codewarse/exercise083.js
@@ -14,5 +14,4 @@
 
 function findNextSquare(sq) {
   let numSq = Math.sqrt(sq);
-  return numSq == Math.trunc(numSq) ? Math.pow(numSq + 1, 2) : -1;
-}
+  return numSq == Math.trunc(numSq) ? Math.pow(numSq + 1, 2) : -1
--- index.js
+++
@@ -1,6 +0,0 @@
-function findNextSquare(sq) {
-  let numSq = Math.sqrt(sq);
-  return numSq == Math.trunc(numSq) ? Math.pow(numSq + 1, 2) : -1;
-}
-
-console.log(findNextSquare(9));
