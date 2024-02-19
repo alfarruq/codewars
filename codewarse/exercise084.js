23c192243f3205109a5354bcd1fb99c5fcef7edf
‘alfarruq’ <mengboyevfarrux01@gmail.com>
2024-02-19 12:17:12 +0500

    6kyu Are they the same?

---
+++ codewarse/exercise084.js
@@ -0,0 +1,57 @@
+// <---------  exercise   --------> =>
+
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+// Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+// <---------  exsample   -------->
+
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+// <---------  solution  -------->
+
+function comp(array1, array2) {
+  if (array1 && array2) {
+    let arr = array1;
+    let result = true;
+    for (let i = 0; i < array2.length; i++) {
+      for (let e = 0; e < arr.length; e++) {
+        if (Math.sqrt(array2[i]) == arr[e]) {
+          result = true;
+          delete arr[e];
+          break;
+        } else result = false;
+      }
+      if (!result) break;
+    }
+
+    if (result) return true;
+    return false;
+  }
+  return false;
+}
+
+let a = [121, 144, 19, 161, 19, 144, 19, 10];
+let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+n;
+
+console.log(comp(a, b));
